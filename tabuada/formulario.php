fb65d1d1897b2ed9b0384de69e32b4f5d011293d
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-30 00:27:17 -0300

    POST

---
+++ tabuada/formulario.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Tabuada</title>
+    </head>
+    <body>
+		<form id="form1" name="form1" method="post" action="tabuada.php">
+			<label for="numero">Número</label>
+			<input type="text" name="numero" id="numero" value="2" />
+			<input type="submit" name="Enviar" id="Enviar" value="Enviar" />
+		</form>
+		<?php
+		?>
+    </body>
+</html>
