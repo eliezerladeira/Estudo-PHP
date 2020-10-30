b80717f77ec82c8bf47f80ef1134de92bab89048
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-29 22:45:43 -0300

    Update operadoreslogicos.php

--- operadoreslogicos.php
+++ operadoreslogicos.php
@@ -8,6 +8,7 @@
 		<?php
 			$n1 = 5;
 			$n2 = 7;
+			$n3 = 6;
 			
 			if ($n1 == $n2) {
 					echo "<h2>O número $n1 é igual ao número $n2</h2>";
@@ -26,6 +27,22 @@
 			}
 			if ($n1 != $n2) {
 					echo "<h2>O número $n1 é diferente do número $n2</h2>";
+			}
+			if (($n1 == $n2) && ($n1 > 10)) {
+					echo "<h2>== e ></h2>";
+			}
+			if (($n1 == $n2) or ($n1 > 10)) {
+					echo "<h2>== ou ></h2>";
+			}
+			
+			if (($n1 + $n2 > $n3) && ($n3 + $n2 > $n1) && ($n1 + $n3 > $n2)) {
+				echo "<h1>Os valores representam os lados de um triângulo.</h1>";
+			} else {
+				echo "<h1>Os valores não representam os lados de um triângulo.</h1>";
+			}
+			
+			if (($n1 > 5) || ($n2 > 5) || ($n3 > 5)) {
+				echo "<h1>Existe pelo menos um valor maior que o número 5.</h1>";
 			}
 		?>
     </body>
