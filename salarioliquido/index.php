43f9ced47c9c44d5fee9ac7bc6fb7627718278c7
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-11-17 22:12:50 -0300

    Salário Líquido

--- salarioliquido/index.php
+++ salarioliquido/index.php
@@ -1,3 +1,15 @@
+<?php
+	$vh = 0;
+	$qh = 0;
+	$pd = 0;
+	
+	if (isset($_GET["vh"]))
+	{
+	$vh = $_GET["vh"];
+	$qh = $_GET["qh"];
+	$pd = $_GET["pd"];
+	}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitionl//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
@@ -8,18 +20,18 @@
 		<form id="form1" name="form1" method="post" action="salarioliquido.php">
 			<p>
 				<label for="qtdhora">Quantidade de horas trabalhadas</label>
-				<input type="text" name="qtdhora" id="qtdhora" />
+				<input type="text" name="qtdhora" id="qtdhora" value="<?php echo $qh; ?>" />
 			</p>
 			<p>
 				<label for="vhoras">Valor da hora trabalhada</label>
-				<input type="text" name="vhoras" id="vhoras" />
+				<input type="text" name="vhoras" id="vhoras" value="<?php echo $vh; ?>" />
 			</p>
 			<p>
 				<label for="desconto">Desconto</label>
 				<select name="desconto" id="desconto">
-					<option value="10">10%</option>
-					<option value="20">20%</option>
-					<option value="30">30%</option>
+					<option value="10" <?php if($pd == 10) {echo 'selected="selected"';} ?>>10%</option>
+					<option value="20" <?php if($pd == 20) {echo 'selected="selected"';} ?>>20%</option>
+					<option value="30" <?php if($pd == 30) {echo 'selected="selected"';} ?>>30%</option>
 				</select>
 			</p>
 			<p>
--- salarioliquido/salarioliquido.php
+++ salarioliquido/salarioliquido.php
@@ -13,18 +13,27 @@
 			$sbruto = 0;
 			$sliquido = 0;
 			
-			if (isset($POST["Enviar"])) {
-				$qhoras = $POST["qtdhoras"];
-				$vhoras = $POST["vhoras"];
-				$pdesconto = $POST["desconto"];
+			if (isset($_POST["Enviar"])) {
+				$qhoras = $_POST["qtdhora"];
+				$vhoras = $_POST["vhoras"];
+				$pdesconto = $_POST["desconto"];
 				
-				$sbruto = $qhoras * vhoras;
+				$sbruto = $qhoras * $vhoras;
 				$desconto = ($sbruto * $pdesconto) / 100;
 				$sliquido = $sbruto - $desconto;
 				?>
-				<h2>Quantidade de horas trabalhadas:</h2>
-				<h2>Valor das horas trabalhadas:</h2>
-				<h2>Percentual de desconto:</h2>
+				<h2>Quantidade de horas trabalhadas: <?php echo $qhoras ?></h2>
+				<h2>Valor das horas trabalhadas: <?php echo $vhoras ?></h2>
+				<h2>Percentual de desconto: <?php echo $pdesconto ?></h2>
+				<h2>Salário Bruto: <?php echo $sbruto ?></h2>
+				<h2>Desconto: <?php echo $desconto ?></h2>
+				<h2>Salário Líquido: <?php echo $sliquido ?></h2>
+				<a href="index.php?vh=<?php echo $vhoras;?>&qh=<?php echo $qhoras;?>&pd=<?php echo $pdesconto;?>Voltar</a>
+				<?php
+			} else {
+				?>
+				<a href="index.php">Informe os valores na página principal</a>
+				<?php
 			}
 		?>
     </body>
