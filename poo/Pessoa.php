83862d7f2b2680011ca8c14ad45a003330dbccbd
eliezerladeira <eliezerladeira@yahoo.com.br>
2021-02-09 10:57:55 -0300

    Update POO

---
+++ poo/Pessoa.php
@@ -0,0 +1,16 @@
+<?php
+
+class Pessoa
+{
+	public $nome;
+	public $site;
+	
+	public function falarNome()	{
+		echo $this->nome;
+	}
+	
+	public function falarSite()	{
+		echo $this->site;
+	}
+}
+
