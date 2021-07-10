ca29f919a85a7ab160790e000a64af483e24f5d6
eliezerladeira <eliezerladeira@yahoo.com.br>
2021-07-10 10:43:40 -0300

    POO Criação de classes

--- poo/index.php
+++ poo/index.php
@@ -1,32 +1,27 @@
 <?php
+	class NomeClasse
+	{
+		var $nome;
+		
+		function Ola()
+		{
+			echo "Ola ".$this->$nome;
+		}
+	}
+?>
 
-require "Pessoa.php";
-require "Programador.php";
-//require_once - permite carregar uma única vez, não sendo preciso dar require em todo o projeto para o mesmo arquivo
-//include - não dá erro fatal, somente warning
-//@ no começo - ignora os warnings
-
-//$uma_pessoa = new Pessoa;
-//$uma_pessoa->nome = "Eliezer";
-//$uma_pessoa->site = "www.eliezerladeira.com.br";
-
-//var_dump($uma_pessoa);
-
-//$uma_pessoa->falarNome();
-//$uma_pessoa->falarSite();
-
-// usando get e set
-
-//$uma_pessoa->setNome("Eliezer");
-
-// usando construtor
-//$uma_pessoa = new Pessoa("Eliezer");
-//echo $uma_pessoa->getNome();
-
-// usando herança
-$programador = new Programador("Eliezer", "PHP");
-
-echo $programador->getNome();
-
-//constantes
-echo $programador::ESPECIE;
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title> Documento sem titulo</title>
+	</head>
+	
+	<body>
+		<?php
+			$obj = new NomeClasse();
+			$obj->nome = "Eliezer";
+			$obj->Ola();
+		?>
+	</body>
+</html>
